3a9352b5ca47667186ff8ed41eaf6e731db3385c
Leandro Shokida <leandroshokida@gmail.com>
2015-11-16 17:36:06 -0300

    Changed page styles

--- app/models/concerns/with_teaser.rb
+++ app/models/concerns/with_teaser.rb
@@ -1,5 +1,11 @@
 module WithTeaser
   def teaser(more_link)
-    description.truncate(70, omission: more_link)
+
+    index = description.index(/[.\n]/)
+    if (index != nil)
+      description[0,index]
+    else
+      description.truncate(70 + more_link.length, omission: ' '+more_link)
+    end
   end
 end
